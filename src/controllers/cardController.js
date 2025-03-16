f9d3e339402e4038ca2c68cd448bb2257337a129
tuantran47 <21110810@student.hcmute.edu.vn>
2025-03-16 21:41:21 +0700

    add to cart and get cart detail

---
+++ src/controllers/cardController.js
@@ -0,0 +1,51 @@
+import { StatusCodes } from "http-status-codes"
+import { cardService } from "~/services/cardService"
+
+
+const getCard = async (req, res) => {
+  const { page, itemsPerPage, search, sort } = req.query
+  const products = await cardService.getCard(page, itemsPerPage, search, sort)
+  res.status(StatusCodes.OK).json(products)
+}
+
+const getDetailCart = async (req, res) => {
+  const cartId = req.params
+  const userId = req.jwtDecoded.id
+  const cart = await cardService.getDetailCart(cartId, userId)
+  res.status(StatusCodes.OK).json(cart)
+
+}
+
+const addToCart = async (req, res) => {
+  const userId = req.jwtDecoded.id
+  const {productId,quantity } = req.body
+  const cart = await cardService.addToCart(userId, productId, quantity )
+  res.status(StatusCodes.CREATED).json(cart)
+}
+
+const updateCard = async (req, res) => {
+  const productId = req.params.id
+  const images = req.files || []
+  const arrayBuffer = images.map(image => image.buffer)
+  const updateCard = await cardService.updateCard(productId, req.body,arrayBuffer)
+  res.status(StatusCodes.OK).json(updateCard)
+}
+
+const deleteCard = async (req, res) => {
+  const productId = req.params.id
+  const deleteCard = await cardService.deleteCard(productId)
+  if (deleteCard.deletedCount === 0 ) {
+    res.status(StatusCodes.OK).json({message: 'Product not found' })
+  } else {
+    res.status(StatusCodes.OK).json({message: 'Deleted product success!' })
+  }
+}
+
+
+export const cardController = {
+  getCard,
+  getDetailCart,
+  addToCart,
+  updateCard,
+  deleteCard
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,16 @@ import express from 'express'
 import { userRoute } from './userRoute'
 import { uploadRoute } from './uploadRoute'
 import { productRoute } from './productRoute'
+import { cardRoute } from './cardRoute'
 
 const Router = express.Router()
 
 Router.use('/users', userRoute)
 Router.use('/uploads', uploadRoute)
 Router.use('/products', productRoute)
+Router.use('/carts', cardRoute)
+
+
 
 
 
